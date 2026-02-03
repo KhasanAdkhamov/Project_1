5ac9a5f92b75f714d48d286de70c362d7e3e48d1
hasan <hasan.adxamov@gmail.com>
2026-02-03 11:11:00 +0300

    Project1 commit 1st

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,2 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class FileReader {
+
+    public ArrayList<String> readFilesContents(String filename) {
+        String path = "./resources/" + filename;
+        try {
+            return new ArrayList<>(Files.readAllLines(Path.of(path)));
+        } catch (IOException e) {
+            System.out.println("Невозможно прочесть файл с отчетом. Возможно файл отсутствует в директории");
+            return new ArrayList<>();
+        }
+    }
 }
