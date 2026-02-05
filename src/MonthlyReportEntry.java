f68bfa637534257a8fae022e201b0bc33f2daebe
hasan <hasan.adxamov@gmail.com>
2026-02-06 00:27:27 +0300

    реализовал методы считывания месячного и годового отчета

--- src/MonthlyReportEntry.java
+++ src/MonthlyReportEntry.java
@@ -1,12 +1,12 @@
 public class MonthlyReportEntry {
     private String itemName;
-    private boolean expense;
+    private boolean isExpense;
     private int quantity;
     private int sumOfOne;
 
     public MonthlyReportEntry(String itemName, boolean expense, int quantity, int sumOfOne) {
         this.itemName = itemName;
-        this.expense = expense;
+        this.isExpense = expense;
         this.quantity = quantity;
         this.sumOfOne = sumOfOne;
     }
@@ -18,7 +18,7 @@ public class MonthlyReportEntry {
     }
 
     public boolean isExpense() {
-        return expense;
+        return isExpense;
     }
 
     public int getQuantity() {
--- src/ReportProcess.java
+++ src/ReportProcess.java
@@ -1,13 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReportProcess {
+
+    public final static String MONTH_NAME = "m.20210";
+    public final static String YEAR_NAME = "y.202";
+    public List<String> monthReports;
 
     // todo продумать хранение данных
 
     public void loadMonthlyReports() {
-
-
+        FileReader fileReader = new FileReader();
+        for (int i = 1; i < 4; i++) {
+            ArrayList<String> readFilesContents= fileReader.readFilesContents(MONTH_NAME + i + ".csv");
+            for (int j = 1; j < readFilesContents.size(); j++) {
+                System.out.println(readFilesContents.get(j));
+            }
+        }
     }
 
     public void loadYearlyReports() {
+        FileReader fileReader = new FileReader();
+        for (int i = 1; i < 2; i++) {
+            ArrayList<String> readFilesContents = fileReader.readFilesContents(YEAR_NAME + i + ".csv");
+            for (int j = 1; j < readFilesContents.size(); j++) {
+                System.out.println(readFilesContents.get(j));
+            }
+        }
+
 
     }
 
