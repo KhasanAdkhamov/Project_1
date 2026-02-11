e2cea329313870ef2a34de1e4784806b26368695
hasan <hasan.adxamov@gmail.com>
2026-02-12 02:44:14 +0300

    реализовал вывод информации по итогам расходов и доходов в сверке

--- src/ReportProcess.java
+++ src/ReportProcess.java
@@ -72,16 +72,21 @@ public class ReportProcess {
         }
         for (MonthlyReport monthlyReport : monthlyReports) {
             System.out.println(monthlyReport.getMonth());
+            int totalSumOfExpense = 0;
+            int totalSumOfIncome = 0;
             for (MonthlyReportEntry entry : monthlyReport.getEntries()) {
                 if (entry.isExpense()) {
                     int expense = monthlyReport.totalSum(entry);
+                    totalSumOfExpense += expense;
                     System.out.println(entry.getItemName() + " " + expense + " расход");
                 } else {
                     int income = monthlyReport.totalSum(entry);
                     System.out.println(entry.getItemName() + " " + income + " доход");
+                    totalSumOfIncome += income;
                 }
             }
-
+            System.out.println(totalSumOfExpense + " общая сумма расходов");
+            System.out.println(totalSumOfIncome + " общая сумма доходов");
         }
     }
 
